b13ef7369300d0ba0e7a9664cae46db3023c323f
Alex Notov <alex@zealoushacker.com>
2015-02-17 08:36:30 -0800

    Students need to be deleted first, due to fk constraint

--- models/squad.rb
+++ models/squad.rb
@@ -28,8 +28,8 @@ class Squad < Model
   end
 
   def destroy
-    Squad.conn.exec('DELETE FROM squads WHERE id = $1', [ id ] )
     Squad.conn.exec('DELETE FROM students WHERE squad_id = $1', [ id ] )
+    Squad.conn.exec('DELETE FROM squads WHERE id = $1', [ id ] )
   end
 
 end
